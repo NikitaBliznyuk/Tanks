8418a37971bbe2d290134739721f55f134415c95
Nikita Bliznyuk <nikbleid96@gmail.com>
2017-10-04 10:11:42 +0300

    Added charging slider.

--- Assets/Scripts/Tank/ShootTest.cs
+++ Assets/Scripts/Tank/ShootTest.cs
@@ -1,12 +1,14 @@
 ﻿using Planet;
 using Tank.Shoot;
 using UnityEngine;
+using UnityEngine.UI;
 
 public class ShootTest : MonoBehaviour
 {
     public Shoot Shoot;
     public Transform Gun;
     public Transform ShootSpawn;
+    public Slider CharginSlider;
 
     public float power;
     public float maxChargeTime;
@@ -17,7 +19,7 @@ public class ShootTest : MonoBehaviour
     {
         if (Input.GetKeyDown(KeyCode.Space))
         {
-            currentCharge = 0.0f;
+            currentCharge = 0.2f;
         } 
         else if (Input.GetKey(KeyCode.Space))
         {
@@ -28,6 +30,9 @@ public class ShootTest : MonoBehaviour
             float t = Mathf.Clamp(currentCharge / maxChargeTime, 0.2f, 1.0f);
             GravityBody instance = Instantiate(Shoot, ShootSpawn.position, Quaternion.identity).GravityBody;
             instance.ThisRigidbody.AddForce(Gun.forward.normalized * power * t, ForceMode.Force);
+            currentCharge = 0.0f;
         }
+
+        CharginSlider.value = Mathf.Clamp(currentCharge, 0.0f, 1.0f);
     }
 }
